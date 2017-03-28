39b14223d7df4962abb59a04c1b619e73f061622
inti0226 <inti0226@INTI-0226.home>
2017-03-28 14:14:18 +0200

    Compte

--- ProxiBanqueSi/src/main/java/fr/adaming/entities/Compte.java
+++ ProxiBanqueSi/src/main/java/fr/adaming/entities/Compte.java
@@ -6,40 +6,44 @@ import java.util.Date;
 public class Compte implements Serializable {
 
 	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+	/**
 	 * attributs de la classe compte
 	 */
-	int id_compte;
-	long numero;
-	double solde;
-	Date date_ouverture;
-	
+	private int id_compte;
+	private long numero;
+	private double solde;
+	private Date date_ouverture;
+
 	/**
 	 * attribut de compte épargne
 	 */
-	long taux;
- 
+	private long taux;
+
 	/**
 	 * attribut de compte courant
 	 */
-	double decouvert;
-	
-	
+	private double decouvert;
+
 	/**
 	 * booléen : épargne (1) ou courant (0)
 	 */
-	boolean type;
-	
-	
+	private boolean type;
+
 	/**
 	 * déclaration des associations
 	 */
-	
+
 	/**
 	 * constructeur compte générique
 	 */
 	/**
-	/**
-	 * constructeur avec id	
+	 * /** constructeur avec id
+	 * 
 	 * @param id_compte
 	 * @param numero
 	 * @param solde
@@ -53,9 +57,9 @@ public class Compte implements Serializable {
 		this.date_ouverture = date_ouverture;
 	}
 
-
 	/**
 	 * constructeur sans id
+	 * 
 	 * @param numero
 	 * @param solde
 	 * @param date_ouverture
@@ -67,9 +71,9 @@ public class Compte implements Serializable {
 		this.date_ouverture = date_ouverture;
 	}
 
-/**
- * constructeur vide
- */
+	/**
+	 * constructeur vide
+	 */
 	public Compte() {
 		super();
 	}
@@ -77,15 +81,16 @@ public class Compte implements Serializable {
 	/**
 	 * constructeur compte courant
 	 */
-/**
- * constructeur avec id
- * @param id_compte
- * @param numero
- * @param solde
- * @param date_ouverture
- * @param decouvert
- * @param type
- */
+	/**
+	 * constructeur avec id
+	 * 
+	 * @param id_compte
+	 * @param numero
+	 * @param solde
+	 * @param date_ouverture
+	 * @param decouvert
+	 * @param type
+	 */
 	public Compte(int id_compte, long numero, double solde, Date date_ouverture, double decouvert, boolean type) {
 		super();
 		this.id_compte = id_compte;
@@ -96,14 +101,15 @@ public class Compte implements Serializable {
 		this.type = type;
 	}
 
-/**
- * constructeur sans id 
- * @param numero
- * @param solde
- * @param date_ouverture
- * @param decouvert
- * @param type
- */
+	/**
+	 * constructeur sans id
+	 * 
+	 * @param numero
+	 * @param solde
+	 * @param date_ouverture
+	 * @param decouvert
+	 * @param type
+	 */
 	public Compte(long numero, double solde, Date date_ouverture, double decouvert, boolean type) {
 		super();
 		this.numero = numero;
@@ -113,11 +119,12 @@ public class Compte implements Serializable {
 		this.type = type;
 	}
 
-/**
- * constructeur compte epargne
- */
+	/**
+	 * constructeur compte epargne
+	 */
 	/**
 	 * constructeur compte epargne sans id
+	 * 
 	 * @param numero
 	 * @param solde
 	 * @param date_ouverture
@@ -133,10 +140,9 @@ public class Compte implements Serializable {
 		this.type = type;
 	}
 
-
-/**
- * constructeur compte epargne avec id
- */
+	/**
+	 * constructeur compte epargne avec id
+	 */
 	public Compte(long numero, double solde, Date date_ouverture, long taux, boolean type) {
 		super();
 		this.numero = numero;
@@ -146,13 +152,115 @@ public class Compte implements Serializable {
 		this.type = type;
 	}
 
+	/**
+	 * @return the id_compte
+	 */
+	public int getId_compte() {
+		return id_compte;
+	}
 
-@Override
-public String toString() {
-	return "Compte [id_compte=" + id_compte + ", numero=" + numero + ", solde=" + solde + ", date_ouverture="
-			+ date_ouverture + ", taux=" + taux + ", decouvert=" + decouvert + ", type=" + type + "]";
-}
+	/**
+	 * @param id_compte
+	 *            the id_compte to set
+	 */
+	public void setId_compte(int id_compte) {
+		this.id_compte = id_compte;
+	}
 
+	/**
+	 * @return the numero
+	 */
+	public long getNumero() {
+		return numero;
+	}
 
-	
+	/**
+	 * @param numero
+	 *            the numero to set
+	 */
+	public void setNumero(long numero) {
+		this.numero = numero;
+	}
+
+	/**
+	 * @return the solde
+	 */
+	public double getSolde() {
+		return solde;
+	}
+
+	/**
+	 * @param solde
+	 *            the solde to set
+	 */
+	public void setSolde(double solde) {
+		this.solde = solde;
+	}
+
+	/**
+	 * @return the date_ouverture
+	 */
+	public Date getDate_ouverture() {
+		return date_ouverture;
+	}
+
+	/**
+	 * @param date_ouverture
+	 *            the date_ouverture to set
+	 */
+	public void setDate_ouverture(Date date_ouverture) {
+		this.date_ouverture = date_ouverture;
+	}
+
+	/**
+	 * @return the taux
+	 */
+	public long getTaux() {
+		return taux;
+	}
+
+	/**
+	 * @param taux
+	 *            the taux to set
+	 */
+	public void setTaux(long taux) {
+		this.taux = taux;
+	}
+
+	/**
+	 * @return the decouvert
+	 */
+	public double getDecouvert() {
+		return decouvert;
+	}
+
+	/**
+	 * @param decouvert
+	 *            the decouvert to set
+	 */
+	public void setDecouvert(double decouvert) {
+		this.decouvert = decouvert;
+	}
+
+	/**
+	 * @return the type
+	 */
+	public boolean isType() {
+		return type;
+	}
+
+	/**
+	 * @param type
+	 *            the type to set
+	 */
+	public void setType(boolean type) {
+		this.type = type;
+	}
+
+	@Override
+	public String toString() {
+		return "Compte [id_compte=" + id_compte + ", numero=" + numero + ", solde=" + solde + ", date_ouverture="
+				+ date_ouverture + ", taux=" + taux + ", decouvert=" + decouvert + ", type=" + type + "]";
+	}
+
 }
