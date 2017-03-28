52627ae5bf56e9772900fd236e8739fd3295df88
inti0277 <inti0277@DESKTOP-QVPF7VB.home>
2017-03-28 13:50:10 +0200

    compte

--- ProxiBanqueSi/src/main/java/fr/adaming/entities/Compte.java
+++ ProxiBanqueSi/src/main/java/fr/adaming/entities/Compte.java
@@ -13,11 +13,31 @@ public class Compte implements Serializable {
 	double solde;
 	Date date_ouverture;
 	
+	/**
+	 * attribut de compte épargne
+	 */
+	long taux;
+ 
+	/**
+	 * attribut de compte courant
+	 */
+	double decouvert;
+	
+	
+	/**
+	 * booléen : épargne (1) ou courant (0)
+	 */
+	boolean type;
+	
 	
 	/**
 	 * déclaration des associations
 	 */
 	
+	/**
+	 * constructeur compte générique
+	 */
+	/**
 	/**
 	 * constructeur avec id	
 	 * @param id_compte
@@ -32,6 +52,7 @@ public class Compte implements Serializable {
 		this.solde = solde;
 		this.date_ouverture = date_ouverture;
 	}
+
 
 	/**
 	 * constructeur sans id
@@ -54,51 +75,84 @@ public class Compte implements Serializable {
 	}
 
 	/**
-	 * get et set
-	 * @return
+	 * constructeur compte courant
 	 */
-public int getId_compte() {
-	return id_compte;
-}
-
-public void setId_compte(int id_compte) {
-	this.id_compte = id_compte;
-}
-
-public long getNumero() {
-	return numero;
-}
-
-public void setNumero(long numero) {
-	this.numero = numero;
-}
-
-public double getSolde() {
-	return solde;
-}
-
-public void setSolde(double solde) {
-	this.solde = solde;
-}
-
-public Date getDate_ouverture() {
-	return date_ouverture;
-}
-
-public void setDate_ouverture(Date date_ouverture) {
-	this.date_ouverture = date_ouverture;
-}
+/**
+ * constructeur avec id
+ * @param id_compte
+ * @param numero
+ * @param solde
+ * @param date_ouverture
+ * @param decouvert
+ * @param type
+ */
+	public Compte(int id_compte, long numero, double solde, Date date_ouverture, double decouvert, boolean type) {
+		super();
+		this.id_compte = id_compte;
+		this.numero = numero;
+		this.solde = solde;
+		this.date_ouverture = date_ouverture;
+		this.decouvert = decouvert;
+		this.type = type;
+	}
 
 /**
- * affichage du résultat
+ * constructeur sans id 
+ * @param numero
+ * @param solde
+ * @param date_ouverture
+ * @param decouvert
+ * @param type
  */
+	public Compte(long numero, double solde, Date date_ouverture, double decouvert, boolean type) {
+		super();
+		this.numero = numero;
+		this.solde = solde;
+		this.date_ouverture = date_ouverture;
+		this.decouvert = decouvert;
+		this.type = type;
+	}
+
+/**
+ * constructeur compte epargne
+ */
+	/**
+	 * constructeur compte epargne sans id
+	 * @param numero
+	 * @param solde
+	 * @param date_ouverture
+	 * @param taux
+	 */
+	public Compte(int id_compte, long numero, double solde, Date date_ouverture, long taux, boolean type) {
+		super();
+		this.id_compte = id_compte;
+		this.numero = numero;
+		this.solde = solde;
+		this.date_ouverture = date_ouverture;
+		this.taux = taux;
+		this.type = type;
+	}
+
+
+/**
+ * constructeur compte epargne avec id
+ */
+	public Compte(long numero, double solde, Date date_ouverture, long taux, boolean type) {
+		super();
+		this.numero = numero;
+		this.solde = solde;
+		this.date_ouverture = date_ouverture;
+		this.taux = taux;
+		this.type = type;
+	}
+
+
 @Override
 public String toString() {
 	return "Compte [id_compte=" + id_compte + ", numero=" + numero + ", solde=" + solde + ", date_ouverture="
-			+ date_ouverture + "]";
+			+ date_ouverture + ", taux=" + taux + ", decouvert=" + decouvert + ", type=" + type + "]";
 }
-	
-	
-	
+
+
 	
 }
