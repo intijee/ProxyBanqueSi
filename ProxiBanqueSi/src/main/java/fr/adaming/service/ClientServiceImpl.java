a05ef9efe08908f4273423c7a29e4210e1599e3d
inti0297 <inti0297@DESKTOP-BLNLL7N.home>
2017-03-28 13:19:12 +0200

    crud client

--- ProxiBanqueSi/src/main/java/fr/adaming/service/ClientServiceImpl.java
+++ ProxiBanqueSi/src/main/java/fr/adaming/service/ClientServiceImpl.java
@@ -2,32 +2,41 @@ package fr.adaming.service;
 
 import java.util.List;
 
+import fr.adaming.dao.IClientDao;
 import fr.adaming.entities.Client;
 
 public class ClientServiceImpl implements IClientService{
+	
+	private IClientDao clientDao;
+	
+	/**
+	 * @param clientDao the clientDao to set
+	 */
+	public void setClientDao(IClientDao clientDao) {
+		this.clientDao = clientDao;
+	}
+	
 
 	public List<Client> getAllClient() {
-		// TODO Auto-generated method stub
-		return null;
+		return clientDao.getAllClient();
 	}
 
 	public Client getByReference(String reference_client) {
-		// TODO Auto-generated method stub
-		return null;
+		return clientDao.getByReference(reference_client);
 	}
 
 	public void addClient(Client client) {
-		// TODO Auto-generated method stub
+		clientDao.addClient(client);
 		
 	}
 
 	public void deleteClient(String reference_client) {
-		// TODO Auto-generated method stub
+		clientDao.deleteClient(reference_client);
 		
 	}
 
 	public void updateClient(Client client) {
-		// TODO Auto-generated method stub
+		clientDao.updateClient(client);
 		
 	}
 
