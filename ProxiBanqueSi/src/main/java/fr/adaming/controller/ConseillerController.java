340357c6287542055e3a30411eaa87fa8df21b34
inti0226 <inti0226@INTI-0226.home>
2017-03-29 10:36:50 +0200

    conseillerCtrl + accueil Conseiller

--- ProxiBanqueSi/src/main/java/fr/adaming/controller/ConseillerController.java
+++ ProxiBanqueSi/src/main/java/fr/adaming/controller/ConseillerController.java
@@ -2,8 +2,14 @@ package fr.adaming.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import fr.adaming.entities.Agence;
+import fr.adaming.entities.Conseiller;
 import fr.adaming.service.IConseillerService;
 
 @Controller
@@ -20,6 +26,37 @@ public class ConseillerController {
 		this.conseillerService = conseillerService;
 	}
 	
+	@RequestMapping(value="/accueilConseiller",method=RequestMethod.GET)
+	public String accueil(ModelMap model){
+		
+		return "conseillerPages/accueil";
+		
+	}
+	
+	@RequestMapping(value="/ajouter",method=RequestMethod.GET)
+	public String ajouter(Model model){
+		
+		model.addAttribute("ajouterForm", new Object[]{new Conseiller(),new Agence()});
+		
+		return "conseillerPages/ajouter";
+	}
+	
+	
+	@RequestMapping(value="/soumettreAjouter",method=RequestMethod.POST)
+	public String soumettreFormulaireAjouter(Model model,@ModelAttribute("ajouterForm") Object[] tab){
+		
+		Conseiller conseiller=(Conseiller) tab[0];
+		
+		Agence agence=(Agence) tab[1];
+		
+		conseiller.setpAgence(agence);
+		
+		conseillerService.addConseiller(conseiller);
+		
+		return "conseillerPages/accueil";
+		
+		
+	}
 	
 	
 
