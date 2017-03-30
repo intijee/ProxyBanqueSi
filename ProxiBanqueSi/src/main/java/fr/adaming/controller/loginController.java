18816d14e275083f61e5ab2d1d48cb177f5a08f1
inti0293 <inti0293@DESKTOP-4C50ANM>
2017-03-30 11:04:36 +0200

    secu part 3

--- ProxiBanqueSi/src/main/java/fr/adaming/controller/loginController.java
+++ ProxiBanqueSi/src/main/java/fr/adaming/controller/loginController.java
@@ -4,16 +4,25 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/login")
 public class loginController {
 
 	@RequestMapping(value = "/loginGerant")
 	public String pageLoginGerant(){
-		return "loginPages/loginPagesGerant";
+		return "loginPagesGerant";
 	}
 	
 	@RequestMapping(value ="/loginConseiller")
 	public String pageLoginConseiller(){
-		return "loginPages/loginPagesConseiller";
+		return "loginPagesConseiller";
+	}
+	
+	@RequestMapping(value ="/logoutConseiller")
+	public String pageLogoutConseiller(){
+		return "index";
+	}
+	
+	@RequestMapping(value ="/logoutGerant")
+	public String pageLogoutGerant(){
+		return "index";
 	}
 }
